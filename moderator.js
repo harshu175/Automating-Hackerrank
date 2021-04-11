4f0d4f12b5b4e168b7aad95375d37c17d99f3027
Harshit Pandey <harshitp175@gmail.com>
2021-04-11 21:25:07 +0530

    Add files via upload

---
+++ moderator.js
@@ -0,0 +1,62 @@
+const pup = require("puppeteer");
+let id = "yaxigas201@art2427.com";
+let pass = "4546#Harshu";
+let challenge = require("./pepchallenge");
+let data = {"First": "harshitp175",
+            "Second" : "Harshit1",
+            "Third" : "Harshit3"};
+
+
+async function main(){
+    let browser = await pup.launch({
+        headless : false,
+        defaultViewport : false,
+        args : ["--start-maximized"]
+    });
+    let pages = await browser.pages();
+    let tab = pages[0];
+    await tab.goto("https://www.hackerrank.com/auth/login?h_l=body_middle_left_button&h_r=login");
+    await tab.type("#input-1", id);
+    await tab.type("#input-2", pass);
+    await tab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
+    await tab.waitForNavigation({waitUntil : "networkidle2"});
+    await tab.click(".dropdown-handle.nav_link.toggle-wrap");
+    await tab.click("a[data-analytics='NavBarProfileDropDownAdministration']");
+    await tab.waitForSelector(".nav-tabs.nav.admin-tabbed-nav li", {visible : true});
+    let link = await tab.$$(".nav-tabs.nav.admin-tabbed-nav li");
+    await link[1].click();
+    await tab.waitForSelector(".backbone.block-center", {visible : true});
+    let temp_challenge_link = await tab.$$(".backbone.block-center");
+    console.log(temp_challenge_link.length);
+
+    for(let i=0; i<temp_challenge_link.length; i++){
+        let challengelink = await tab.evaluate(function(ele){
+            return ele.getAttribute("href");
+        },temp_challenge_link[i]);
+        
+        addModerator("https://www.hackerrank.com"+challengelink, await browser.newPage());
+        
+    }
+}
+async function addModerator(url , tab){
+    await tab.goto(url);
+    
+    await tab.waitForSelector("li[data-tab='moderators']", {visible: true});
+    await tab.click("li[data-tab='moderators']");
+    
+    if (await tab.$("#cancelBtn") !== null){
+        await tab.waitForSelector("#cancelBtn", {visible: true});
+        await tab.click("#cancelBtn");
+    }
+    
+    await tab.waitForSelector("#moderator", {visible : true});
+
+    await tab.type("#moderator", data["First"]);
+    await tab.keyboard.press("Enter");
+    await tab.type("#moderator", data["Second"]);
+    await tab.keyboard.press("Enter");
+    await tab.type("#moderator", data["Third"]);
+    await tab.keyboard.press("Enter");
+}
+
+main();
---
+++ script.js
@@ -0,0 +1,152 @@
+const pup = require("puppeteer");
+let id = "yaxigas201@art2427.com";
+let pass = "4546#Harshu";
+
+let browserPromise = pup.launch({
+    headless: false,
+    defaultViewport : false
+});
+let tab;
+browserPromise.then(function(browser){
+    let pagepromise= browser.pages();
+    return pagepromise;
+
+}).then(function(pages){
+    tab = pages[0];
+    let pageOpenPromise = tab.goto("https://www.hackerrank.com/auth/login?h_l=body_middle_left_button&h_r=login");
+    return pageOpenPromise;
+
+}).then(function(){
+    let idPromise = tab.type("#input-1", id);
+    return idPromise;
+
+}).then(function(){
+    let passPromise = tab.type("#input-2", pass);
+    return passPromise;
+
+}).then(function(){
+    let loginPromise = tab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
+    return loginPromise;
+
+}).then(function(){
+    let waitPromise = tab.waitForSelector("#base-card-1-link", {visible : true});
+    return waitPromise;
+
+}).then(function(){
+    let ipClickPromise = tab.click("#base-card-1-link" );
+    return ipClickPromise;
+
+}).then(function(){
+    let waitPromise2 = tab.waitForSelector("a[data-attr1='warmup']" , {visible : true});
+    return waitPromise2;
+
+}).then(function(){
+    let warmUpPromise = tab.click("a[data-attr1='warmup']");
+    return warmUpPromise;
+
+}).then(function(){
+    let waitPromise3 = tab.waitForSelector(".js-track-click.challenge-list-item", {visible : true});
+    return waitPromise3;
+
+}).then(function(){
+    let solveAddUrlPromise = tab.$$(".js-track-click.challenge-list-item");
+    return solveAddUrlPromise;
+
+}).then(function(data){
+    let urlPromise = [];
+    for(let i=0; i<data.length; i++){
+        let fetchUrl = tab.evaluate(function(ele){
+           return ele.getAttribute('href');
+        },data[i]);
+        urlPromise.push(fetchUrl);
+    }
+    return Promise.all(urlPromise);
+
+}).then(function(data){
+    let solvePromise = solveProblem("https://www.hackerrank.com" + data[0]);
+    for(let i=1; i<data.length; i++){
+        solvePromise = solvePromise.then(function(){
+            return solveProblem("https://www.hackerrank.com" + data[i]);
+        })
+    }
+
+}).catch(function(err){
+    console.log("error occured");
+});
+
+function solveProblem(url){
+    let problemUrl = url;
+    let editorial = url.replace("?" , "/editorial?");
+    
+    return new Promise(function(resolve, reject){
+        tab.goto(editorial).then(function(){
+            let languagePromise =  tab.$$(".hackdown-content h3");
+            return languagePromise;
+        }).then(function(data){
+            let languagePromise = [];
+            for(let i of data){
+                let languagesPromise = tab.evaluate(function(ele){
+                    return ele.textContent;
+                },i);
+                languagePromise.push(languagesPromise);
+            }
+            return Promise.all(languagePromise);
+
+        }).then(function(data){
+            // console.log(data.length);
+            for(let i in data){
+                if(data[i] == "C++"){
+                    let finalAnsPromise = tab.$$(".highlight").then(function(answer){
+                        let answerPromise = tab.evaluate(function(ele){
+                            return ele.textContent;
+                        },answer[i]);
+                        return answerPromise;
+                    });
+                    return finalAnsPromise;
+                }
+            }
+        }).then(function(data){
+            return tab.goto(problemUrl).then(function(){
+                let checkboxWaitPromise = tab.waitForSelector(".custom-input-checkbox" , {visible : true});
+                return checkboxWaitPromise;
+            }).then(function(){
+                let checkboxclickPromise = tab.click(".custom-input-checkbox");
+                return checkboxclickPromise;
+            }).then(function(){
+                let answerTypePromise = tab.type(".custominput" , data);
+                return answerTypePromise;
+            }).then(function(){
+                let controlDownPromise = tab.keyboard.down("Control");
+                return controlDownPromise;
+            }).then(function(){
+                let pressApromise = tab.keyboard.press("A");
+                return pressApromise;
+            }).then(function(){
+                let pressXpromise = tab.keyboard.press("X");
+                return pressXpromise;
+            }).then(function(){
+                let editorClickPromise = tab.click(".monaco-scrollable-element.editor-scrollable.vs");
+                return editorClickPromise;
+            }).then(function(){
+                let pressApromise = tab.keyboard.press("A");
+                return pressApromise;
+            }).then(function(){
+                let pressVpromise = tab.keyboard.press("V");
+                return pressVpromise;
+            }).then(function(){
+                let unpressCntrl = tab.keyboard.up("Control");
+                return unpressCntrl;
+            }).then(function(){
+                return tab.click(".pull-right.btn.btn-primary.hr-monaco-submit");
+            }).then(function(){
+                return tab.waitForSelector(".congrats-wrapper" , {visible : true});
+            }).catch(function(err){
+                console.log("Error");
+            })
+        }).then(function(){
+            resolve();
+        }).catch(function(err){
+            console.log("Error");
+        });
+    })
+}   
